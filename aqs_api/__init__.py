118118398342a08fb2fe7031149d915973dab09e
srobe <srobe@umd.edu>
2022-08-22 17:01:10 -0400

    Create __init__.py

---
+++ aqs_api/__init__.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import requests
+import json
+import datetime as dt
+
+__all__ = ['aqs_login','readin','utils']
+
+from . import readin
+from . import utils 
+from . import aqs_login
