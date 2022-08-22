74f8128b4409bde7f57e98fa54e5363236d3eaa3
srobe <srobe@umd.edu>
2022-08-22 17:10:59 -0400

    Create mdstate_O3.py

---
+++ examples/mdstate_O3.py
@@ -0,0 +1,20 @@
+## Uncomment lines below if you've updated the aqs_api modules
+#import sys
+#for k in list(sys.modules):
+#    if k.startswith('aqs_api'):
+#        del sys.modules[k]
+
+from aqs_api import *
+from aqs_api.readin import get_aqs_data
+
+
+args_in = { 
+    'param':'44201', # Ozone
+    'bdate':'20100101', #Start Date
+    'edate':'20121231', #End Date
+    'state':'24', # Maryland
+  }
+
+file_out = get_aqs_data('sampleData','byState',**args_in)
+
+
